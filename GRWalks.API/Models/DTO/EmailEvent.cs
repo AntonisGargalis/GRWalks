2a00c27225ed687a144870b7a13cd3f4a26fefca
AntonisGargalis <antonis7gar@gmail.com>
2025-10-18 19:25:51 +0300

    minor add

---
+++ GRWalks.API/Models/DTO/EmailEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace GRWalks.API.Models.DTO
+{
+    public class EmailEvent
+    {
+        public string? To { get; set; }
+        public string? Subject { get; set; }
+        public string? Body { get; set; }
+    }
+}
